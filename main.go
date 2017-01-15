c7d984d846ef6e23ec1328c289cb048e4051a0d1
Mr. Is <isaac.ardis@gmail.com>
2017-01-15 10:14:43 +0100

    uniqifier

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-
 	"github.com/rotblauer/bildRoam/bildRoam"
+	"io"
+	"os/exec"
 	// "log"
 	"encoding/csv"
 	"os"
@@ -22,14 +24,16 @@ func floatToString(input float64) string {
 
 func main() {
 	var output string
-	var iphotosDir string
+	var targetDir string
 	var myName string
+	var uniqify bool
 
 	// go run main.go -dir ~/Desktop/locs/ -out ~/Desktop/locs/whois.csv -name jl
 
 	flag.StringVar(&output, "out", "iwazhere.csv", "specify the output .csv file")
-	flag.StringVar(&iphotosDir, "dir", "/Users/ia/Pictures/iphoto-export", "directory with photos")
+	flag.StringVar(&targetDir, "dir", "/Users/ia/Pictures/Photos Library.photoslibrary/Masters", "directory with photos")
 	flag.StringVar(&myName, "name", "ia", "your tag name")
+	flag.BoolVar(&uniqify, "uniq", true, "make the output csv pipe through sort -u after")
 
 	flag.Parse()
 
@@ -41,7 +45,7 @@ func main() {
 
 	writer := csv.NewWriter(file)
 
-	filepath.Walk(iphotosDir, func(path string, info os.FileInfo, err error) error {
+	filepath.Walk(targetDir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
@@ -66,4 +70,31 @@ func main() {
 
 	defer writer.Flush()
 
+	if uniqify {
+		uniq := exec.Command("sort", "-u", output)
+		upipe, e := uniq.StdoutPipe()
+		if e != nil {
+			fmt.Println(upipe)
+		}
+
+		// open the tmp out file for writing
+		tmpout, err := os.Create("./sortedtmp.csv")
+		if err != nil {
+			panic(err)
+		}
+		defer tmpout.Close()
+
+		writer := bufio.NewWriter(tmpout)
+
+		es := uniq.Start()
+		if es != nil {
+			fmt.Println(es)
+		}
+
+		go io.Copy(writer, upipe)
+		uniq.Wait()
+
+		// now once our sorting is complete, we'll make tmpout the real output
+		os.Rename("./sortedtmp.csv", output)
+	}
 }
